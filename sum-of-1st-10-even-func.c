4d7f40b2274ae4c0207bc22a435145f7a373fd87
Alok <alokshrestha12386@gmail.com>
2025-01-02 15:18:33 +0545

    hello

--- sum-of-1st-10-even-func.c
+++ sum-of-1st-10-even-func.c
@@ -11,6 +11,26 @@ int alok(){
 int main(){
     int sum = alok();
 
- printf("The sum is %d",sum);
+ printf("The sum of first 10 even number is %d",sum);
  return 0;
 }
+
+
+// #include<stdio.h>
+// int alok(){
+//  int i,sum =0;
+//  for(i = 1;i<=10;i++){
+    
+//         sum = sum +(2*i);
+    
+//  }
+//  return sum;
+// }
+// int main(){
+//     int sum = alok();
+
+//  printf("The sum of first 10 even number is %d",sum);
+//  return 0;
+// }
+
+
