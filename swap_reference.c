e108b994a1e50d286d0f182a7f53071f39f45009
Alok stha <alokshrestha12386@gmail.com>
2025-01-23 15:01:33 +0545

    hello

---
+++ swap_reference.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void swap(int *a,int *b){
+ int t = *a;
+ *a=*b;
+ *b=t;
+ printf("Inside swap:value of a is %d and b is %d",*a,*b);
+ printf("\n");
+}
+int main(){
+    int a = 10;
+    int b = 20;
+    swap(&a,&b);
+    printf("Inside main:value of a is %d and b is %d",a,b);
+    return 0;
+}
+
+
+
